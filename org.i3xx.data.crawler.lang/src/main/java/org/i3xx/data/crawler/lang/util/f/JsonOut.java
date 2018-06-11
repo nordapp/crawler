a639a29d294a34458032ce58825cfed504b95286
green <green@entwicklungii>
2018-06-11 12:16:48 +0200

    Some decisitions

---
+++ org.i3xx.data.crawler.lang/src/main/java/org/i3xx/data/crawler/lang/util/f/JsonOut.java
@@ -0,0 +1,66 @@
+package org.i3xx.data.crawler.lang.util.f;
+
+import org.i3xx.data.crawler.lang.core.Function;
+import org.i3xx.data.crawler.lang.core.LeafNode;
+import org.i3xx.data.crawler.lang.core.LeafNodeImpl;
+import org.i3xx.data.crawler.lang.core.ListNode;
+import org.i3xx.data.crawler.lang.core.Node;
+import org.i3xx.data.crawler.lang.core.Node.Type;
+
+public class JsonOut implements Function {
+	
+	public static final String NAME = "jsonmp";
+	
+	@Override
+	public Function getInstance() {
+		return this;
+	}
+	
+	@Override
+	public Node exec(Node node, boolean fix) throws Exception {
+		
+		StringBuffer buf = new StringBuffer();
+		print(node, buf, 0);
+		
+		return new LeafNodeImpl(Type.NODE, fix?NAME:node.getName(), buf.toString());
+	}
+	
+	private void print(Node node, StringBuffer buf, int depth) {
+		
+		if(node instanceof ListNode) {
+			
+			if(depth>0 && node.getName()!=null) {
+				buf.append('"');
+				buf.append(node.getName());
+				buf.append('"');
+				buf.append(':');
+			}
+			
+			ListNode list = (ListNode)node;
+			for(int i=0;i<list.getStruct().size();i++) {
+				Node child = list.getStruct().get(i);
+				if(i==0) {
+					buf.append('{');
+				}else {
+					buf.append(',');
+				}//
+				
+				print(child, buf, depth+1);
+				
+				if(i==(list.getStruct().size()-1)) {
+					buf.append('}');
+				}//fi
+			}//for
+		}else {
+			if(depth>0 && node.getName()!=null) {
+				buf.append('"');
+				buf.append(node.getName());
+				buf.append('"');
+				buf.append(':');
+				buf.append('"');
+				buf.append( ((LeafNode)node).getValue() );
+				buf.append('"');
+			}
+		}
+	}
+}
---
+++ org.i3xx.data.crawler.lang/src/test/java/org/i3xx/data/crawler/lang/core/JsonOutTest.java
@@ -0,0 +1,30 @@
+package org.i3xx.data.crawler.lang.core;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JsonOutTest {
+
+	@Test
+	void testA() throws Exception {
+		ListNode list = new ListNodeImpl();
+		String stmt = "";
+		
+		stmt += "a {\"person\": {\"first-name\":\"John\", \"name\":\"Doe\", \"test\":7}}\n";
+		stmt += "jsonm a\n";
+		stmt += "jsonmp a\n";
+		
+		TupleParser p = new TupleParser();
+		p.parse(list, stmt);
+
+		TupleResolver r = new TupleResolver();
+		Node n = r.resolveAndGetLast(list);
+		
+		assertTrue( n instanceof LeafNodeImpl );
+		assertEquals( n.getName(), "a");
+		//Caution: The value is "7" because there are only String values allowed.
+		assertEquals( ((LeafNode)n).getValue(), "{\"person\":{\"first-name\":\"John\",\"name\":\"Doe\",\"test\":\"7\"}}");
+	}
+
+}
