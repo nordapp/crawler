86b493f3d5fedde3f26bc20180b5dac6a1450b99
green <green@entwicklungii>
2018-06-15 19:44:41 +0200

    Add test

---
+++ org.i3xx.data.crawler.lang/src/test/java/org/i3xx/data/crawler/lang/core/OffLiteralTest.java
@@ -0,0 +1,31 @@
+package org.i3xx.data.crawler.lang.core;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class OffLiteralTest {
+
+	@Test
+	void testA() throws Exception {
+		
+		ListNode list = new ListNodeImpl();
+		String stmt = "";
+		
+		stmt += "a \"Test\"\n";
+		stmt += "offliteral a\n";
+		
+		TupleParser p = new TupleParser();
+		p.parse(list, stmt);
+		
+		TupleResolver r = new TupleResolver();
+		Node n = r.resolveAndGetLast(list);
+		
+		assertTrue( n instanceof LeafNodeImpl );
+		assertEquals( n.getName(), "a");
+		assertEquals( ((LeafNode)n).getValue(), "Test");
+		
+	}
+
+}
